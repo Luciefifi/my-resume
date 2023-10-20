ef7bd6d951e7050b59bf785e6f71ca93967502ce
Luciefifi <niyomutonilucie@gmail.com>
2023-10-20 10:54:30 +0300

    y cv

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
+import {AiOutlineMail, AiFillGithub} from 'react-icons/ai'
+import {BsFillTelephoneFill} from 'react-icons/bs'
+import {ImLocation} from 'react-icons/im'
+import {FaGraduationCap} from 'react-icons/fa'
+import {GoDotFill} from 'react-icons/go'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="">
+      <div className="bg-green-500 h-[100vh] w-[20%]"> 
+      <img src="/assets/mypic.jpg" className=""/>
+      <div className="m-4">
+        <div className='mb-6'>
+        <p className=" ml-8 text-2xl">
+          Contacts
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="flex flex-row items-center space-x-4 mb-2">
+
+          <AiOutlineMail />
+        <p>niyomutonilucie@gmail.com</p>
+        </div>
+        <div className="flex flex-row items-center space-x-4 mb-2">
+          <BsFillTelephoneFill />
+          <p>+250788864959</p>
+
+        </div>
+        
+        <div className="flex flex-row items-center space-x-4 mb-2">
+
+          <AiFillGithub />
+          <p>Luciefifi</p>
+
+        </div>
+        
+        <div className="flex flex-row items-center space-x-4 mb-2">
+
+          <ImLocation />
+          <p>Kigali-Rwanda</p>
+        </div>
+        </div>
+        <div>
+        <div className="flex flex-row items-center space-x-4 mb-2 text-3xl">
+
+<FaGraduationCap  />
+<p>Education</p>
+</div>
+<div className="flex flex-row items-center space-x-4 mb-2">
+
+<GoDotFill />
+<p>University of Rwanda</p> 
+</div>
+        </div>
+      </div>
+      </div>
+
     </div>
   );
 }
